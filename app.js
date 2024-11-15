2f2363bcfda868a3b6ea619af1867c1f74c883f2
sz05 <shreyans0528@gmail.com>
2024-11-15 16:35:59 +0530

    new features , added distance

--- app.js
+++ app.js
@@ -1,25 +1,93 @@
 const express = require("express");
 const cors = require("cors");
-const axios = require("axios");
 const bodyParser = require("body-parser");
-const { firestore } = require("./utils/firebase-admin");
-
 const http = require("http");
+const socketIo = require("socket.io");
+const { v4: uuidv4 } = require("uuid");
+const jwt = require("jsonwebtoken");
+const verifyToken = require("./middlewares/auth");
+const verifySocketToken = require("./middlewares/socketAuth");
+const { firestore } = require("./utils/firebase-admin");
 
 const app = express();
 const port = 5500;
 
 const server = http.createServer(app);
 
-const ORS_API_KEY = "5b3ce3597851110001cf62483def44912c544f018818c7750ac93839";
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+  },
+  path: "/ws",
+});
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// socket
+io.use(verifySocketToken);
+
+io.on("connection", (socket) => {
+  console.log("A user connected", socket.user);
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
+const ORS_API_KEY = "5b3ce3597851110001cf62483def44912c544f018818c7750ac93839";
+
 app.get("/", (req, res) => {
   res.json({ message: "Hello World!" });
+});
+
+app.post("/hardware/:store_id", verifyToken, async (req, res) => {
+  try {
+    const { store_id } = req.params;
+    const userId = req.user.uid;
+    console.log(userId);
+
+    const storeDoc = await firestore.collection("stores").doc(store_id).get();
+    if (!storeDoc.exists || storeDoc.data().created_by !== userId) {
+      return res
+        .status(403)
+        .json({ error: "Store does not belong to the user" });
+    }
+
+    const hardwareQuerySnapshot = await firestore
+      .collection("hardware")
+      .where("store_id", "==", store_id)
+      .get();
+    if (!hardwareQuerySnapshot.empty) {
+      return res
+        .status(400)
+        .json({ error: "Hardware already exists for this store" });
+    }
+
+    const client_id = uuidv4();
+    const client_secret = jwt.sign({ client_id }, "your_jwt_secret", {
+      expiresIn: "730h",
+    });
+
+    const hardwareData = {
+      client_id,
+      client_secret,
+      store_id,
+      created_at: new Date().toISOString(),
+    };
+
+    const docRef = await firestore.collection("hardware").add(hardwareData);
+    res.json({ message: "Data added successfully", data: hardwareData });
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        error: "Error adding data to Firestore",
+        details: error.message,
+      });
+  }
 });
 
 app.post("/api/distance-matrix", async (req, res) => {
@@ -30,8 +98,8 @@ app.post("/api/distance-matrix", async (req, res) => {
   const data = {
     locations: [...origins, ...destinations],
     metrics: ["distance", "duration"],
-    sources: Array.from(Array(origins.length).keys()),
-    destinations: Array.from(Array(destinations.length).keys()),
+    sources: [0],
+    destinations: [1],
   };
 
   try {
@@ -41,6 +109,7 @@ app.post("/api/distance-matrix", async (req, res) => {
         "Content-Type": "application/json",
       },
     });
+    console.log(response.data);
     res.json(response.data);
   } catch (error) {
     console.error("Error fetching data from ORS:", error.message);
---
+++ utils/hardware.js
@@ -0,0 +1,8 @@
+const getHardwareData = (client_id, client_secret, store_id, created_at) => {
+  return {
+    client_id,
+    client_secret,
+    store_id,
+    created_at,
+  };
+};
