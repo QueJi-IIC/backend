a552a459490152679fe47413b819d7c4c0bdc842
Yashraj Pahwa <59451283+yashrajpahwa@users.noreply.github.com>
2024-11-15 21:16:28 +0530

    segregation of files

---
+++ socketHandler.js
@@ -0,0 +1,27 @@
+const { Socket } = require("socket.io");
+const { firestore } = require("./utils/firebase-admin");
+
+/**
+ * @param {Socket} socket
+ */
+const socketHandler = (socket) => {
+  console.log("A user connected", socket.store_id);
+  socket.on("detection_status", async (event) => {
+    try {
+      const statusData = {
+        status: event.detected,
+        timestamp: new Date().toISOString(),
+      };
+      console.log(statusData);
+      await firestore.collection("status").doc(socket.store_id).set(statusData, { merge: true });
+      console.log("Status updated in Firestore:", statusData);
+    } catch (error) {
+      console.error("Error updating status in Firestore:", error);
+    }
+  });
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+};
+
+module.exports = socketHandler;
